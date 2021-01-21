8eaed0c9d5310700407d7f4207e2d6190cf87b85
Réau <anaisreau@gmail.com>
2021-01-21 14:32:20 +0100

    heroku

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
-// const ContenuRouter = require("./routes/contenuRouter");
-// const MatiereRouter = require("./routes/matiereRouter");
-// const TypeContenuRouter = require("./routes/typeContenuRouter");
-// const RoleRouter = require("./routes/Role");
-// const NiveauRouter = require("./routes/niveauContenuRouter");
-// const VisioRouter = require("./routes/visioConferences.router");
-// const Role = db.role;
+const ContenuRouter = require("./routes/contenuRouter");
+const MatiereRouter = require("./routes/matiereRouter");
+const TypeContenuRouter = require("./routes/typeContenuRouter");
+const RoleRouter = require("./routes/Role");
+const NiveauRouter = require("./routes/niveauContenuRouter");
+const VisioRouter = require("./routes/visioConferences.router");
+const Role = db.role;
 
 
 app.use(cors());
@@ -25,29 +25,29 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Anais Reau Application" });
 });
 
-// require('./routes/auth.routes')(app);
-// require('./routes/user.routes')(app);
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
-// app.use('/api/contenus', ContenuRouter)
-// app.use('/api/matieres', MatiereRouter)
-// app.use('/api/types', TypeContenuRouter)
-// app.use('/api/roles', RoleRouter);
-// app.use('/api/niveaux', NiveauRouter)
-// app.use('/api/visios', VisioRouter)
+app.use('/api/contenus', ContenuRouter)
+app.use('/api/matieres', MatiereRouter)
+app.use('/api/types', TypeContenuRouter)
+app.use('/api/roles', RoleRouter);
+app.use('/api/niveaux', NiveauRouter)
+app.use('/api/visios', VisioRouter)
 
 
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "user"
-//   });
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
  
-//   Role.create({
-//     id: 2,
-//     name: "moderator"
-//   });
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
 
-// }
+}
 // set port, listen for requests
 const port = process.env.PORT || 8080;
 
