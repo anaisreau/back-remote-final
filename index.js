77d19921bacdfa7f36aa9206e803d7ffcdd32878
Réau <anaisreau@gmail.com>
2021-01-22 09:46:17 +0100

    cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Role = models.role;
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://remote-inc-api.herokuapp.com/api/user/1');
+  res.setHeader('Access-Control-Allow-Origin', 'https://remote-inc-api.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
