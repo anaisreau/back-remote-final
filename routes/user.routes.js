83516598a84d38515ed3d7bcdc1365b41295d36b
Réau <anaisreau@gmail.com>
2021-01-22 11:32:48 +0100

    prout

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,22 +1,14 @@
 const { authJwt } = require("../middlewares");
 const controller = require("../controllers/user.controller");
-
-
+const express = require('express');
+const app = express();
+app.use(cors())
 module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
-  // app.get("/api/test/all", controller.allAccess);
-
-  // app.get("/api/test/user", [authJwt.verifyToken], );
-
-  // app.get("/api/test/mod",[authJwt.verifyToken, authJwt.isModerator], controller.moderatorBoard);
-
-  // app.get("/api/test/admin", [authJwt.verifyToken, authJwt.isAdmin],  controller.adminBoard);
-
 
 // favorites 
   app.post("/api/user/favorites/add", controller.addFavorite);
