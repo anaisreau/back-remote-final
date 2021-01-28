16154f3b157c82b27e7aac41df8153ca0f11870e
Réau <anaisreau@gmail.com>
2021-01-28 14:47:39 +0100

    req-id

--- middlewares/authjwt.js
+++ middlewares/authjwt.js
@@ -2,6 +2,7 @@
 const jwt = require("jsonwebtoken");
 const db = require("../models");
 const User = db.user;
+
 const  secret= "anaisnananasecretkey"
 
 verifyToken = (req, res, next) => {
@@ -18,67 +19,12 @@ verifyToken = (req, res, next) => {
         message: "Mot de passe invalide"
       });
     }
-    req.id = decoded.id;
+    req.user_id = decoded.id;
     next();
   });
 };
 
-// isAdmin = (req, res, next) => {
-//   User.findByPk(req.userId).then(user => {
-//     user.getRoles().then(roles => {
-//       for (let i = 0; i < roles.length; i++) {
-//         if (roles[i].name === "admin") {
-//           next();
-//           return;
-//         }
-//       }
 
-//       res.status(403).send({
-//         message: "Require Admin Role!"
-//       });
-//       return;
-//     });
-//   });
-// };
-
-// isModerator = (req, res, next) => {
-//   User.findByPk(req.userId).then(user => {
-//     user.getRoles().then(roles => {
-//       for (let i = 0; i < roles.length; i++) {
-//         if (roles[i].name === "moderator") {
-//           next();
-//           return;
-//         }
-//       }
-
-//       res.status(403).send({
-//         message: "Require Moderator Role!"
-//       });
-//     });
-//   });
-// };
-
-// isModeratorOrAdmin = (req, res, next) => {
-//   User.findByPk(req.userId).then(user => {
-//     user.getRoles().then(roles => {
-//       for (let i = 0; i < roles.length; i++) {
-//         if (roles[i].name === "moderator") {
-//           next();
-//           return;
-//         }
-
-//         if (roles[i].name === "admin") {
-//           next();
-//           return;
-//         }
-//       }
-
-//       res.status(403).send({
-//         message: "Require Moderator or Admin Role!"
-//       });
-//     });
-//   });
-// };
 
 const authJwt = {
   verifyToken: verifyToken,
