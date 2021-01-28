6cef911cd99ebf9fa5e3717701c5f6342ba04f54
Réau <anaisreau@gmail.com>
2021-01-28 15:38:20 +0100

    acces control allow oriin

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,7 @@ const User = express.Router();
 
   User.use(function(req, res, next) {
     res.header(
-      "Access-Control-Allow-Origin: *", 
+      "Access-Control-Allow-Origin",
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
