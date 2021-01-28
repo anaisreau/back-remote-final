d39e252917e12f5d41f18bcc8e045b35fe65b375
Réau <anaisreau@gmail.com>
2021-01-28 17:55:43 +0100

    acces control allow methods

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,9 @@ const Auth = express.Router();
 Auth.use(function(req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
-    "x-access-token, Origin, Content-Type, Accept"
+    "x-access-token, Origin, Content-Type, Accept",
+    'Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS'
+
   );
   next();
 });
