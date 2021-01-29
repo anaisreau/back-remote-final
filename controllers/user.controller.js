84c300d5ccfdee645d242d2d08c4cf7b411bf100
Réau <anaisreau@gmail.com>
2021-01-29 10:03:00 +0100

    password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,7 +57,7 @@ exports.modifyUser = (req, res)=> {
    firstname : firstname,
    lastname :  lastname,
    email :  email,
-   password : bcrypt.hashSync(password, 8),
+   password : password,
    phone : phone,
    description : description,
    roleId : roleId
